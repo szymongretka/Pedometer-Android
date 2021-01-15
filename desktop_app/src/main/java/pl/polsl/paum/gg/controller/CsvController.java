ede1bf777b45f3d6b52ef02b3eeb44e6c097e168
Myslawek <przemek.g96@wp.pl>
2021-01-15 20:49:23 +0100

    Poprawa nazewnictwa

--- desktop_app/src/main/java/pl/polsl/paum/gg/controller/CsvController.java
+++
@@ -1,9 +0,0 @@
-package pl.polsl.paum.gg.controller;
-
-import com.opencsv.CSVParser;
-import com.opencsv.CSVParserBuilder;
-
-public class CsvController {
-	private final CSVParser csvParser = new CSVParserBuilder().withSeparator(',').build();
-	
-}
---
+++ desktop_app/src/main/java/pl/polsl/paum/gg/controller/StackPaneController.java
@@ -0,0 +1,7 @@
+package pl.polsl.paum.gg.controller;
+
+import javafx.stage.FileChooser;
+
+public class StackPaneController {
+
+}
