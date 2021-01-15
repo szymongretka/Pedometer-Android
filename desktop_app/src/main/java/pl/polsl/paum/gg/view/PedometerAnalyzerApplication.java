bf2185e7a125f3a4fdd65e4f9e67350cb03b8989
Myslawek <przemek.g96@wp.pl>
2021-01-15 20:46:33 +0100

    Zmiana tworzenia UI w kodzie na fxml

--- desktop_app/src/main/java/pl/polsl/paum/gg/view/PedometerAnalyzerApplication.java
+++ desktop_app/src/main/java/pl/polsl/paum/gg/view/PedometerAnalyzerApplication.java
@@ -3,13 +3,22 @@ package pl.polsl.paum.gg.view;
 import java.io.IOException;
 
 import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
+
 public class PedometerAnalyzerApplication extends Application {
 
 	@Override
 	public void start(Stage stage) throws IOException {
+		FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/StackPaneWindow.fxml"));
+		StackPane stackPane = loader.load();
+
+		Scene scene = new Scene(stackPane);
+		stage.setScene(scene);
+		stage.setTitle("PedometerAnalyzer");
+		stage.show();
 	}
 
 	public void run(String[] args) {
