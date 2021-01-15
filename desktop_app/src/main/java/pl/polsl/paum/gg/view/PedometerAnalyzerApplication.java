5945b49fd0c87d8b593c3dec6f1c1a2c07a29eab
Myslawek <przemek.g96@wp.pl>
2021-01-15 20:45:18 +0100

    Zmiana nazewnictwa

---
+++ desktop_app/src/main/java/pl/polsl/paum/gg/view/PedometerAnalyzerApplication.java
@@ -0,0 +1,19 @@
+package pl.polsl.paum.gg.view;
+
+import java.io.IOException;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+public class PedometerAnalyzerApplication extends Application {
+
+	@Override
+	public void start(Stage stage) throws IOException {
+	}
+
+	public void run(String[] args) {
+		launch(args);
+	}
+
+}
