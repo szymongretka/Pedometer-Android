2dd91f95cab145cbe035529675ae2731eff3991c
Myslawek <przemek.g96@wp.pl>
2021-01-15 22:38:42 +0100

    Zmiana konfiguracji UI

--- desktop_app/src/main/java/pl/polsl/paum/gg/controller/MainController.java
+++ desktop_app/src/main/java/pl/polsl/paum/gg/controller/MainController.java
@@ -1,43 +1,40 @@
 package pl.polsl.paum.gg.controller;
 
-import java.io.BufferedReader;
 import java.io.File;
-import java.io.FileInputStream;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
-import java.nio.file.Files;
 
 import javafx.fxml.FXML;
 import javafx.scene.control.Alert;
 import javafx.scene.control.Alert.AlertType;
 import javafx.scene.input.MouseEvent;
-import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.FileChooser;
 import javafx.stage.FileChooser.ExtensionFilter;
+import javafx.stage.Window;
+import pl.polsl.paum.gg.bind.CsvUnmarshaller;
 import pl.polsl.paum.gg.bind.impl.CsvUnmarshallerImpl;
 import pl.polsl.paum.gg.exception.ConversionException;
 import pl.polsl.paum.gg.model.PedometerCsv;
-import javafx.stage.Window;
 
-public class StackPaneController {
-	
-	private final CsvUnmarshallerImpl csvUnmarshaller = new CsvUnmarshallerImpl();
+public class MainController {
+
+	private final CsvUnmarshaller csvUnmarshaller = new CsvUnmarshallerImpl();
 	private PedometerCsv pedometerCsv = null;
 
 	@FXML
 	private Window window;
 
 	@FXML
-	private StackPane stackPaneMain;
+	private VBox vboxMain;
 
-	
 	@FXML
 	private void initialize() {
-		//act as constructor
+		// act as constructor
 	}
 
 	@FXML
-	public void onButtonCsvClicked(MouseEvent event) {
+	public void onMenuItemOpenCsvAction() {
 		File sourceFile;
 		FileChooser fileChooser = new FileChooser();
 		fileChooser.setTitle("Open Pedometer CSV File");
@@ -51,9 +48,9 @@ public class StackPaneController {
 		} catch (ConversionException e) {
 			showWarning("Zawartoœæ wskazanego pliku jest uszkodzona i nie uda³o siê jej przetworzyæ.");
 		}
-		
+
 	}
-	
+
 	private void showWarning(String textToDisplay) {
 		Alert alert = new Alert(AlertType.WARNING, textToDisplay);
 		alert.getDialogPane().setHeaderText("Wyst¹pi³ b³¹d");
--- desktop_app/src/main/java/pl/polsl/paum/gg/view/PedometerAnalyzerApplication.java
+++ desktop_app/src/main/java/pl/polsl/paum/gg/view/PedometerAnalyzerApplication.java
@@ -6,16 +6,17 @@ import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
 import javafx.scene.layout.StackPane;
+import javafx.scene.layout.VBox;
 import javafx.stage.Stage;
 
 public class PedometerAnalyzerApplication extends Application {
 
 	@Override
 	public void start(Stage stage) throws IOException {
-		FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/StackPaneWindow.fxml"));
-		StackPane stackPane = loader.load();
+		FXMLLoader loader = new FXMLLoader(this.getClass().getResource("/fxml/MainWindow.fxml"));
+		VBox vBox = loader.load();
 
-		Scene scene = new Scene(stackPane);
+		Scene scene = new Scene(vBox);
 		stage.setScene(scene);
 		stage.setTitle("PedometerAnalyzer");
 		stage.show();
