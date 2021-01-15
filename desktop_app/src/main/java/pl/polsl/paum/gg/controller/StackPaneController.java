30edd38d64e15a502fb8f788a4238e51b86eb918
Myslawek <przemek.g96@wp.pl>
2021-01-15 22:12:19 +0100

    Dodanie odczytania pliku i jego konwersji

--- desktop_app/src/main/java/pl/polsl/paum/gg/controller/StackPaneController.java
+++ desktop_app/src/main/java/pl/polsl/paum/gg/controller/StackPaneController.java
@@ -1,7 +1,63 @@
 package pl.polsl.paum.gg.controller;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.nio.file.Files;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Alert;
+import javafx.scene.control.Alert.AlertType;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.StackPane;
 import javafx.stage.FileChooser;
+import javafx.stage.FileChooser.ExtensionFilter;
+import pl.polsl.paum.gg.bind.impl.CsvUnmarshallerImpl;
+import pl.polsl.paum.gg.exception.ConversionException;
+import pl.polsl.paum.gg.model.PedometerCsv;
+import javafx.stage.Window;
 
 public class StackPaneController {
+	
+	private final CsvUnmarshallerImpl csvUnmarshaller = new CsvUnmarshallerImpl();
+	private PedometerCsv pedometerCsv = null;
+
+	@FXML
+	private Window window;
+
+	@FXML
+	private StackPane stackPaneMain;
+
+	
+	@FXML
+	private void initialize() {
+		//act as constructor
+	}
+
+	@FXML
+	public void onButtonCsvClicked(MouseEvent event) {
+		File sourceFile;
+		FileChooser fileChooser = new FileChooser();
+		fileChooser.setTitle("Open Pedometer CSV File");
+		fileChooser.setInitialFileName("data.csv");
+		fileChooser.getExtensionFilters().add(new ExtensionFilter("CSV files (*.csv)", "*.csv"));
+		sourceFile = fileChooser.showOpenDialog(window);
+		try {
+			pedometerCsv = csvUnmarshaller.unmarshalCsvToPojo(new FileReader(sourceFile));
+		} catch (FileNotFoundException e) {
+			showWarning("Nie uda³o siê otworzyæ podanego pliku.");
+		} catch (ConversionException e) {
+			showWarning("Zawartoœæ wskazanego pliku jest uszkodzona i nie uda³o siê jej przetworzyæ.");
+		}
+		
+	}
+	
+	private void showWarning(String textToDisplay) {
+		Alert alert = new Alert(AlertType.WARNING, textToDisplay);
+		alert.getDialogPane().setHeaderText("Wyst¹pi³ b³¹d");
+		alert.showAndWait();
+	}
 
 }
