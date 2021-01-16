6ff796b397c246d4f33b335bd1e5e1afe3fdf8aa
Myslawek <przemek.g96@wp.pl>
2021-01-16 14:33:03 +0100

    Dodanie wizualizacji dla tygodni i miesięcy

--- desktop_app/src/main/java/pl/polsl/paum/gg/controller/MainController.java
+++ desktop_app/src/main/java/pl/polsl/paum/gg/controller/MainController.java
@@ -4,6 +4,7 @@ import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import java.time.Duration;
+import java.time.LocalDate;
 import java.time.LocalTime;
 import java.time.temporal.ChronoUnit;
 import java.util.LinkedList;
@@ -31,8 +32,9 @@ import pl.polsl.paum.gg.repository.impl.PedometerCsvRepositoryImpl;
 
 public class MainController {
 
-	private final static int HOUR_SEPARATOR = 2; // show chart point every HOUR_SEPARATOR (sum all entities within
+	private final static long HOURS_PER_POINT = 2; // show chart point every HOUR_SEPARATOR (sum all entities within
 													// HOUR_SEPARATOR duration)
+	private final static long DAYS_PER_POINT = 1;
 	private final CsvUnmarshaller csvUnmarshaller = new CsvUnmarshallerImpl();
 	private final PedometerCsvRepository repository = new PedometerCsvRepositoryImpl();
 
@@ -114,9 +116,9 @@ public class MainController {
 		}
 		String unit = comboBoxUnit.getValue();
 		areaChartSteps.setTitle(unit);
+		XYChart.Series<String, Integer> series = new XYChart.Series<>();
 		if ("Dzieñ".equals(unit)) {
 			categoryAxisUnit.setLabel("Godziny");
-			XYChart.Series<String, Integer> series = new XYChart.Series<>();
 			series.setName("Kroki w godzinie");
 			DailyStepRecord dailyStepRecord = repository
 					.findDailyStepRecordByLocalDate(datePickerStartingDay.getValue());
@@ -124,20 +126,27 @@ public class MainController {
 				showWarning("Nie znaleziono danych w podanym dniu.");
 				return;
 			}
-			series.getData().setAll(generateHourPoints(Duration.ofHours(HOUR_SEPARATOR), dailyStepRecord));
-			areaChartSteps.setAnimated(false);
-			areaChartSteps.getData().clear();
-			areaChartSteps.getData().add(series);
+			series.getData().setAll(generateHoursPoints(Duration.ofHours(HOURS_PER_POINT), dailyStepRecord));
 		} else if ("Tydzieñ".equals(unit)) {
 			categoryAxisUnit.setLabel("Dni");
+			series.setName("Kroki w dni tygodnia");
+			List<DailyStepRecord> dailyStepRecordList = repository.findAllDailyStepRecords();
+			series.getData().setAll(generateDaysPoints(Duration.ofDays(DAYS_PER_POINT), dailyStepRecordList,
+					datePickerStartingDay.getValue(), 7));
 		} else {
 			categoryAxisUnit.setLabel("Dni");
-//			XYChart.Data<String, Integer> x = new XYChart.Data<>("1:00", steps);
+			series.setName("Kroki w dni miesi¹ca");
+			List<DailyStepRecord> dailyStepRecordList = repository.findAllDailyStepRecords();
+			series.getData().setAll(generateDaysPoints(Duration.ofDays(DAYS_PER_POINT), dailyStepRecordList,
+					datePickerStartingDay.getValue(), 30));
 		}
+		areaChartSteps.setAnimated(false);
+		areaChartSteps.getData().clear();
+		areaChartSteps.getData().add(series);
 
 	}
 
-	private List<XYChart.Data<String, Integer>> generateHourPoints(Duration durationPerPoint,
+	private List<XYChart.Data<String, Integer>> generateHoursPoints(Duration durationPerPoint,
 			DailyStepRecord dailyRecord) {
 		// null safe
 		List<XYChart.Data<String, Integer>> result = new LinkedList<>();
@@ -163,6 +172,44 @@ public class MainController {
 					stepRecord.getTime().truncatedTo(ChronoUnit.MINUTES).toString(), stepSum));
 		}
 		return result;
+	}
+
+	private List<XYChart.Data<String, Integer>> generateDaysPoints(Duration daysPerPoint,
+			List<DailyStepRecord> dailyStepRecordList, LocalDate startingDay, int daysIntervalSize) {
+		// null safe
+		List<XYChart.Data<String, Integer>> result = new LinkedList<>();
+		DailyStepRecord dailyStepRecord;
+		Duration currentDuration = Duration.ofSeconds(0);
+		LocalDate currentLocalDate = startingDay;
+		int stepsSum = 0;
+		for (int i = 0; i < daysIntervalSize; ++i) {
+			currentDuration = currentDuration.plus(Duration.ofDays(1));
+			dailyStepRecord = repository.findDailyStepRecordByLocalDate(currentLocalDate);
+			stepsSum += sumStepsInDay(dailyStepRecord);
+			if (currentDuration.compareTo(daysPerPoint) >= 0) {
+				result.add(new XYChart.Data<String, Integer>(
+						currentLocalDate.getDayOfMonth() + "." + currentLocalDate.getMonthValue(), stepsSum));
+				currentDuration = Duration.ofSeconds(0);
+				stepsSum = 0;
+			}
+			currentLocalDate = currentLocalDate.plusDays(1L);
+		}
+		if (!currentDuration.isZero()) {
+			LocalDate date = currentLocalDate.minusDays(1L);
+			result.add(new XYChart.Data<String, Integer>(date.getDayOfMonth() + "." + date.getMonthValue(), stepsSum));
+		}
+		return result;
+	}
+
+	private int sumStepsInDay(DailyStepRecord day) {
+		if (day == null || day.getStepRecordList() == null) {
+			return 0;
+		}
+		int dayStepsSum = 0;
+		for (StepRecord stepRecord : day.getStepRecordList()) {
+			dayStepsSum += stepRecord.getStepsAmount();
+		}
+		return dayStepsSum;
 	}
 
 	private void showWarning(String textToDisplay) {
